eccad15ce3641ff1b0e86f868a4fba329d3a90e4
cleanDev-win <beautifulsky1991@hotmail.com>
2020-07-20 22:59:26 +0300

    fix some errors

--- demo/src/home.vue
+++ demo/src/home.vue
@@ -8,26 +8,12 @@
     </div>
     <div class="row">
       <div class="col-md-12">
-        <vue-webrtc
-          ref="webrtc"
-          width="100%"
-          :roomId="roomId"
-          v-on:joined-room="logEvent"
-          v-on:left-room="logEvent"
-          v-on:opened-room="logEvent"
-          v-on:share-started="logEvent"
-          v-on:share-stopped="logEvent"
-          @error="onError"
-        />
+        <vue-webrtc ref="webrtc" width="100%" :roomId="roomId" @error="onError" />
       </div>
       <div class="row">
         <div class="col-md-12 my-3">
-          <button type="button" class="btn btn-primary" @click="onJoin">
-            On
-          </button>
-          <button type="button" class="btn btn-danger" @click="onLeave">
-            Off
-          </button>
+          <button type="button" class="btn btn-primary" @click="onJoin">On</button>
+          <button type="button" class="btn btn-danger" @click="onLeave">Off</button>
         </div>
       </div>
     </div>
@@ -35,12 +21,8 @@
       <div class="col-md-12 my-3">
         <h3>Comments list</h3>
       </div>
-      <div v-if="loading" class="loading">
-        Loading...
-      </div>
-      <div v-if="error" class="error">
-        {{ error }}
-      </div>
+      <div v-if="loading" class="loading">Loading...</div>
+      <div v-if="error" class="error">{{ error }}</div>
       <div v-if="comments" class="content">
         <div v-for="comment in comments" :key="comment.id">
           <div class="d-flex align-items-center">
@@ -71,22 +53,20 @@ export default {
       roomId: "public-room",
       loading: false,
       comments: [],
-      error: null,
+      error: null
     };
   },
   mounted() {
     this.comment = this.error = null;
     this.loading = true;
     fetch("https://jsonplaceholder.typicode.com/comments")
-      .then((res) => res.json())
-      .then((result) => {
-        console.log("result->", result);
+      .then(res => res.json())
+      .then(result => {
         this.comments = result;
         this.loading = false;
         this.error = null;
       })
-      .catch((error) => {
-        console.log("error->", error);
+      .catch(error => {
         this.error = error.toString();
       });
   },
@@ -97,6 +77,9 @@ export default {
     onLeave() {
       this.$refs.webrtc.leave();
     },
-  },
+    onError(error, stream) {
+      console.log("On Error Event", error, stream);
+    }
+  }
 };
 </script>
